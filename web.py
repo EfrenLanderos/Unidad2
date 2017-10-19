01191f0b604e30234361a58ccb0b9366e7ae4c6e
EfrenLanderos <32500613+EfrenLanderos@users.noreply.github.com>
2017-10-19 18:25:46 -0500

    Add files via upload

--- web.py
+++ web.py
@@ -5,8 +5,8 @@ app = Flask(__name__)
 
 
 @app.route('/')
-def hello_world():
-    return 'Hello World!'
+def hello_world()->'302':
+    return redirect('/entry')
 
 @app.route('/entry')
 def go_entry() -> 'html':
